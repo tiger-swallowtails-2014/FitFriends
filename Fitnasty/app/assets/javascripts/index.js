dbe83f18b889d11e4758caf4544b486d9e75c2aa
annakalkanis <a.kalkanis@gmail.com>
2014-09-26 10:30:00 -0700

    remove test unit and set up rspec

---
+++ Fitnasty/app/assets/javascripts/index.js
@@ -0,0 +1,7 @@
+$.getJSON('/challenges', function(data){
+
+})
+
+var ChallengeItem = Backbone.Model.extend({})
+
+var acceptedItem = new ChallengeItem({"id":1,"first_name":"Oma","last_name":"Koss","email":"lorenz@rempel.name","password_digest":"$2a$10$gfAkPsbim157Q.vGt9YgTO7BRFjD3SNwGNtoEru/3SziSzVayVrPa","created_at":"2014-09-26T03:12:57.530Z","updated_at":"2014-09-26T03:12:57.530Z"})
