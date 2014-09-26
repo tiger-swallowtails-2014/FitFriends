d7f480ee3754a906748f8b8a8c7adf501bda75f7
Nicholas Cantelmi <ncantelmi@Nicholass-MacBook-Air.local>
2014-09-26 16:42:37 -0700

    Separated lightbox javascript into a new folder and individual files, but it isn't working.  Will need to check this after meeting.

--- Fitnasty/app/assets/javascripts/lightbox.js
+++ Fitnasty/app/assets/javascripts/lightbox.js
@@ -1,20 +1,20 @@
-$(document).ready(function() {
-  // View
-  var showSignupLightbox = function() {
-    $('.lightbox_background, .signup_lightbox').fadeIn('slow')
-  }
+// $(document).ready(function() {
+//   // View
+//   var showSignupLightbox = function() {
+//     $('.lightbox_background, .signup_lightbox').fadeIn('slow')
+//   }
 
-  var showLoginLightbox = function() {
-    $('.lightbox_background, .login_lightbox').fadeIn('slow')
-  }
+//   var showLoginLightbox = function() {
+//     $('.lightbox_background, .login_lightbox').fadeIn('slow')
+//   }
 
-  var hide_lightboxes = function() {
-    $('.lightbox_background, .signup_lightbox, .login_lightbox').hide()
-  }
+//   var hide_lightboxes = function() {
+//     $('.lightbox_background, .signup_lightbox, .login_lightbox').hide()
+//   }
 
-  // Controller
-  hide_lightboxes()
-  $('.signup_button').click(showSignupLightbox)
-  $('.login_button').click(showLoginLightbox)
-  $('.lightbox_background').click(hide_lightboxes)
-})
+//   // Controller
+//   hide_lightboxes()
+//   $('.signup_button').click(showSignupLightbox)
+//   $('.login_button').click(showLoginLightbox)
+//   $('.lightbox_background').click(hide_lightboxes)
+// })
