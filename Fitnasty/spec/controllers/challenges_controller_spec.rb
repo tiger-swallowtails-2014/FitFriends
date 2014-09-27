37da895d23139be5088e5d32a3db83c4d86029bd
Gary Hammell <garyhammell@Garys-MacBook-Pro.local>
2014-09-26 18:35:01 -0700

    add search tests to confirm a search yields matching objects based on title, description, and tags

---
+++ Fitnasty/spec/controllers/challenges_controller_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe ChallengesController do
+  describe 'challenge#search' do
+    before(:each) do
+      Challenge.destroy_all
+    end
+
+    let(:challenge) {Challenge.create(title: "test_title", location: "test_location", description: "test_description")}
+    let(:tag) {challenge.tags.create(name: "test_tag")}
+
+    it 'returns a challenge matched by title' do
+      get :search, keyword: challenge.title
+      result = JSON.parse(response.body)[0]
+      expect(result["challenge_object"]["title"]).to eq(challenge.title)
+    end
+
+    it 'returns a challenge matched by description' do
+      get :search, keyword: challenge.description
+      result = JSON.parse(response.body)[0]
+      expect(result["challenge_object"]["description"]).to eq(challenge.description)
+    end
+
+    it 'returns a challenge matched by a tag' do
+      get :search, keyword: tag.name
+      result = JSON.parse(response.body)[0]
+      expect(result["challenge_object"]["title"]).to eq(challenge.title)
+    end
+  end
+end
