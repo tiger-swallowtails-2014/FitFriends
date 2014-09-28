dfea33b2b9af2efe628d3be2dd38d984871bf610
Travis75 <travis.gibson75@gmail.com>
2014-09-28 10:21:16 -0700

    fixed display problem with challenge widget

--- Fitnasty/app/assets/javascripts/challege_widget/controller.js
+++ Fitnasty/app/assets/javascripts/challege_widget/controller.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  var stubbed_data = [{id: 1, user_id: 2, title: "Running Golden Gate", location: "San Francisco", description: "Run the bridge in the Morning, it is so beautiful.", image_url: "http:stuff", created_at: "When Created", updated_at: "When Updated"}, {id: 5, user_id: 10, title: "Go Bike Marin County", location: "Marin", description: "Riding Bikes is super fun and makes you look really cool to people.", image_url: "http:stuff", created_at: "When Created", updated_at: "When Updated"}]
+  var stubbed_data = [{id: 1, user_id: 2, title: "Running Golden Gate", location: "San Francisco", description: "Run the bridge in the Morning, it is so beautiful.", image_url: "http:stuff", created_at: "When Created", updated_at: "When Updated"}, {id: 5, user_id: 10, title: "Go Bike Marin County", location: "Marin", description: "Riding Bikes is super fun and makes you look really cool to people.", image_url: "http:stuff", created_at: "When Created", updated_at: "When Updated"},{id: 5, user_id: 10, title: "Go Bike Marin County", location: "Marin", description: "Riding Bikes is super fun and makes you look really cool to people.", image_url: "http:stuff", created_at: "When Created", updated_at: "When Updated"}]
   var testWidget = new ChallengeWidget();
   testWidget.whenDone(stubbed_data)
 })
@@ -8,17 +8,12 @@ function ChallengeWidget() {
   this.whenDone = function(data) {
     ChallengeFactory.createChallenges(data)
     challengeHolder.challenges
-    console.log(challengeHolder.challenges)
-    var data_style ={challenges: challengeHolder.challenges, user: [{first_name: "travis"}, {first_name: "Evan"}]}
-
-
-    Handlebars.registerHelper('bold', function(options) {
-  return new Handlebars.SafeString(
-      '<div class="mybold">'
-      + options.fn(this)
-      + '</div>');
-  });
-
+    var data_style = {challenges: challengeHolder.challenges, users: [{first_name: "Travis"}, {first_name: "Evan"}]}
+    console.log(HandlebarsTemplates['Challenge_Widget/index'](data_style))
     $("#challenges-container").append(HandlebarsTemplates['Challenge_Widget/index'](data_style))
   }
 }
+
+    Handlebars.registerHelper('getuser', function(options) {
+        return new Handlebars.SafeString(options.fn(options.data.root.users[$(options.data.root.challenges).index(this)]));
+    });
