1009b4aa3152f6c6a8e723d9a9e9e7d155d7264e
Nicholas Cantelmi <ncantelmi@Nicholass-MacBook-Air.local>
2014-09-28 11:18:28 -0700

    Added more styling.  Also, added conditional in the users_controller for the create method.  Renders nothing if the user is not saved(invalid)

--- Fitnasty/app/controllers/users_controller.rb
+++ Fitnasty/app/controllers/users_controller.rb
@@ -13,7 +13,9 @@ class UsersController < ApplicationController
 		user = User.new(user_params)
 		if user.save
 			session[:user_id] = user.id
-			redirect_to "/users/#{user.id}"
+			redirect_to user_path(user)
+		else
+			render nothing: true
 		end
 	end
 
