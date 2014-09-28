7fa88c19b338fb711a277b0949fb196bbb434eef
Travis75 <travis.gibson75@gmail.com>
2014-09-28 10:54:37 -0700

    commit to pull master

--- Fitnasty/app/assets/javascripts/challege_widget/controller.js
+++ Fitnasty/app/assets/javascripts/challege_widget/controller.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
   var stubbed_data = [{id: 1, user_id: 2, title: "Running Golden Gate", location: "San Francisco", description: "Run the bridge in the Morning, it is so beautiful.", image_url: "http:stuff", created_at: "When Created", updated_at: "When Updated"}, {id: 5, user_id: 10, title: "Go Bike Marin County", location: "Marin", description: "Riding Bikes is super fun and makes you look really cool to people.", image_url: "http:stuff", created_at: "When Created", updated_at: "When Updated"},{id: 5, user_id: 10, title: "Go Bike Marin County", location: "Marin", description: "Riding Bikes is super fun and makes you look really cool to people.", image_url: "http:stuff", created_at: "When Created", updated_at: "When Updated"}]
 
   var testWidget = new ChallengeWidget();
-
   testWidget.whenDone(stubbed_data)
 })
 
