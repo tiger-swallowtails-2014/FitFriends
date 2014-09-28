7ee0956d5856f46b077015cb70c7e2dbcb760928
annakalkanis <a.kalkanis@gmail.com>
2014-09-27 20:22:45 -0700

    Remove line in users controller create that returns the user

--- Fitnasty/app/controllers/users_controller.rb
+++ Fitnasty/app/controllers/users_controller.rb
@@ -13,7 +13,6 @@ class UsersController < ApplicationController
 		user = User.new(user_params)
 		if user.save
 			session[:user_id] = user.id
-			return user
 			redirect_to "/users/#{user.id}"
 		end
 	end
