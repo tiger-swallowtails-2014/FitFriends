8a22704a37b59bc6c0439a10119da4e06fb3c329
annakalkanis <a.kalkanis@gmail.com>
2014-09-28 20:26:14 -0700

    resolve merge conflict

--- Fitnasty/config/routes.rb
+++ Fitnasty/config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get '/trends' => "tags#trends", as: :trends
   resources :challenges
   get '/users/current' => "users#current", as: :current
+  get '/users/single_user/:id' => "users#single_user", as: :single_user
 
 
 
