3ef0a1193d16d849aa67e3df51a7734e1003aa1d
Gary Hammell <garyhammell@Garys-MacBook-Pro.local>
2014-09-28 23:43:52 -0700

    merge master

--- Fitnasty/app/assets/javascripts/tabs.js
+++ Fitnasty/app/assets/javascripts/tabs.js
@@ -1,7 +1,6 @@
 var bindUsersTabEvent = function() {
   var shownUser = $(document.URL.split('/')).last()[0]
   $('#user_tab').on('click', function(e){
-    $('#challenges').css("display", "none")
     $.ajax({
       url: "/users/show_follow",
       method: "GET",
