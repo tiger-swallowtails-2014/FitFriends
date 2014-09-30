37c15f26be7b2fb81c1bd18e3107d4c0cd0643f2
Nicholas Cantelmi <ncantelmi@Nicholass-MacBook-Air.local>
2014-09-30 16:35:35 -0700

    Default value for challenge image added

--- Fitnasty/db/seeds.rb
+++ Fitnasty/db/seeds.rb
@@ -11,7 +11,7 @@ class ChallengeSeeder
     40.times do
       lat = rand(38) + 1
       long = rand(123) + 1
-      challenge = User.find(rand(10) + 1).challenges.create(title: Faker::App.name, location: Faker::Address.street_address, description: Faker::Lorem.sentence(15), image_url: "http://upload.wikimedia.org/wikipedia/commons/a/a4/Ggb_by_night.jpg", latitude: (lat), longitude: (long))
+      challenge = User.find(rand(10) + 1).challenges.create(title: Faker::App.name, location: Faker::Address.street_address, description: Faker::Lorem.sentence(15), latitude: (lat), longitude: (long))
       2.times do
         challenge.tags.create(name: Faker::App.name)
       end
