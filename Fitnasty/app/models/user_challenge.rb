bded2559ef82695e12a62b00749d4082bd3378b9
annakalkanis <a.kalkanis@gmail.com>
2014-09-30 11:45:15 -0700

    Add scopes to user_challenge model to pull out accepted and completed

--- Fitnasty/app/models/user_challenge.rb
+++ Fitnasty/app/models/user_challenge.rb
@@ -1,5 +1,8 @@
 class UserChallenge < ActiveRecord::Base
   belongs_to :user
   belongs_to :challenge
+
+  scope :accepted, -> { where(accepted?: true) }
+  scope :completed, -> { where(completed?: true) }
   
 end
