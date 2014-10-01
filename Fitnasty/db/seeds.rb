a8bae61d05f40985e2b3009fa0e7fa76e1269de6
Gary Hammell <garyhammell@Garys-MacBook-Pro.local>
2014-10-01 11:03:39 -0700

    update userChallenge model to only allow unique relationships between user_id and challenge_id

--- Fitnasty/db/seeds.rb
+++ Fitnasty/db/seeds.rb
@@ -1,6 +1,6 @@
 class UserSeeder
   def self.seed
-    10.times do
+    15.times do
       User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, password: "password", email: Faker::Internet.email)
     end
   end
@@ -23,17 +23,22 @@ end
 
 class AcceptedSeeder
   def self.seed
-    20.times do
+    100.times do
+      # to seed accepted and completed challenges
       User.find(rand(10) + 1).user_challenges.create(challenge_id: rand(40) + 1, accepted?: true, completed?: [true, false].sample)
+      # to seed pending challenges
+      User.find(rand(10) + 1).user_challenges.create(challenge_id: rand(40) + 1, accepted?: false, completed?: false)
     end
   end
 end
 
 class FollowSeeder
   def self.seed
-    20.times do
-      User.find(rand(10) + 1).followers << User.find(rand(10)+1)
-      User.find(rand(10) + 1).followees << User.find(rand(10)+1)
+    User.all.each do |user|
+      random_follower = User.find(rand(10)+1)
+      random_followee = User.find(rand(10)+1)
+      user.followers << random_follower unless (user.followers.include?(random_follower) || random_follower.id == user.id)
+      user.followees << random_followee unless (user.followees.include?(random_followee) || random_followee.id == user.id)
     end
   end
 end
